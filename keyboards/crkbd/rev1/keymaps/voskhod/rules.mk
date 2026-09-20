1816a9823b55af6bb511e2e2bc8d6ea310be4d97
Neeeasy <nizerdota@ya.ru>
2026-09-20 18:45:56 +0500

    Create rules.mk

---
+++ keyboards/crkbd/rev1/keymaps/voskhod/rules.mk
@@ -0,0 +1,13 @@
+# Обязательно: плата Corne рассчитана на atmega32u4,
+# а у тебя ProMicro RP2040 — это конвертер.
+CONVERT_TO = promicro_rp2040
+
+RGB_MATRIX_ENABLE = yes
+OLED_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+
+EXTRAKEY_ENABLE = yes
+
+VIA_ENABLE = no
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
