c5f61e18c61b3a91e71cd8aecfd45450a00a21bb
Neeeasy <nizerdota@ya.ru>
2026-09-20 18:59:32 +0500

    Update keymap.c

--- keyboards/crkbd/rev1/keymaps/voskhod/keymap.c
+++ keyboards/crkbd/rev1/keymaps/voskhod/keymap.c
@@ -59,10 +59,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_NAV] = LAYOUT_split_3x6_3(
         KC_LGUI, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_VOLU,
-                 KC_WH_U, KC_HOME, KC_UP,   KC_END,  KC_BSPC, KC_DEL,
+                 KC_NO,   KC_HOME, KC_UP,   KC_END,  KC_BSPC, KC_DEL,
 
         KC_TAB,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_MUTE,
-                 KC_WH_D, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, KC_PSCR,
+                 KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, KC_PSCR,
 
         KC_LSFT, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_VOLD,
                  KC_NO,   KC_MPRV, KC_MPLY, KC_MNXT, KC_PGDN, KC_RALT,
